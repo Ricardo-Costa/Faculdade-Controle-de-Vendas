3d5197fb23195b3a1b8a586b844b00d8fb2eb2e8
Ricardo Costa <ricardo18acs@gmail.com>
2016-04-05 00:20:54 -0300

    Adicionando componentes
    Formulários e máscaras de validações.

---
+++ js/customize.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    /*
+     * Usando plugin "Masked Input"
+     * https://github.com/digitalBush/jquery.maskedinput
+     * */
+    $("#form-cd-us-telefone").mask("(999) 99999-9999");
+
+});
