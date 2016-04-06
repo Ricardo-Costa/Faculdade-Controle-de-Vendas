39b2a50701305f939747f268ad5c4839ba2f287f
Ricardo Costa <ricardo18acs@gmail.com>
2016-04-06 01:07:32 -0300

    Adicionando função - editarUsuario
    Adicionando Função de Edição de dados do usuário no painel de Gerentes.

--- js/funcoes.gerente.js
+++ js/funcoes.gerente.js
@@ -8,7 +8,68 @@
  * @param id - Identificador do usuário
  */
 function editarUsuario(id) {
+    var htmlFomulario =
+        '<p>Alterar dados deste usuário.</p><br/>' +
+        '<form class="form-horizontal" data-toggle="validator" role="form">' +
+        '<div class="form-group">' +
+        '<label for="form-cd-us-tipo" class="control-label col-sm-3"> Tipo </label>' +
+        '<div class="col-sm-7">' +
+        '<select id="form-cd-us-tipo" name="tipo" class="form-control" required="required">' +
+        '<option value="1">Caixeiro</option>' +
+        '<option selected="selected" value="2">Gestor de Compras</option>' +
+        '<option value="3">Gestor de Estoque</option>' +
+        '<option value="4">Gerente</option>' +
+        '</select>' +
+        '<small class="descricao">Selecione o perfil de usuário.</small>' +
+        '</div>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label class="control-label col-sm-3" for="form-cd-us-nome"> Nome </label>' +
+        '<div class="col-sm-7">' +
+        '<input id="form-cd-us-nome" class="form-control" name="nome" data-minlength="3" data-error="Mínimo 3 e máximo 20 caracteres alfanuméricos" maxlength="20" placeholder="Nome de Usuário" required pattern="[A-Za-zÀ-ú, -,Ã,ã,Õ,õ,ç,Ç]{3,20}" title="Mínimo 3 e máximo 20 caracteres alfanuméricos" value="João da Silva"/>' +
+        '<span class="help-block with-errors"></span>' +
+        '</div>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label class="control-label col-sm-3" for="form-cd-us-email"> E-mail </label>' +
+        '<div class="col-sm-7">' +
+        '<input id="form-cd-us-email" class="form-control" type="email" name="email" placeholder="Email do usuário" required="required" value="joao.silva@mail.com"/>' +
+        '<span class="help-block with-errors"></span>' +
+        '</div>' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label class="control-label col-sm-3" for="form-cd-us-telefone"> Telefone </label>' +
+        '<div class="col-sm-7">' +
+        '<input id="form-alt-us-telefone" class="form-control" name="telefone" placeholder="Telefone" required="required" value="085986741234"/>' +
+        '<span class="help-block with-errors"></span>' +
+        '</div></div></div>' +
+        '</form>';
 
+    bootbox.dialog({
+        title: "João Silva <small>(Gestor de Compras)</small>",
+        message: htmlFomulario,
+        buttons: {
+            cancel: {
+                label: "Cancelar",
+                className: "btn-default",
+                callback: function() {
+                    console.log("As alteracoes para este usuario forao canceladas");
+                }
+            },
+            main: {
+                label: "Atualizar",
+                className: "btn-primary",
+                callback: function() {
+                    console.log("Novos dados deverao ser setados");
+                }
+            }
+        }
+    });
+
+    // aplicar máscara
+    $("#form-alt-us-telefone").mask("(999) 99999-9999");
+
+    delete (htmlFomulario);
 }
 
 /**
